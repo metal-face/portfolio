84b30f48cbceaf83cdbae144a7d8bb391329a216
metal-face <hughesbryan3000@gmail.com>
2024-09-11 22:26:05 -0500

    Add express server and static file serving

---
+++ server.ts
@@ -0,0 +1,7 @@
+import { createRequestHandler } from "@remix-run/express";
+import express from "express";
+
+import * as build from "./build/server/index.js";
+
+const app = express();
+app.use(express.static("build/client"));
