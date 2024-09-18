c10807c91fed2203a434b0e25654232466b1c8f1
metal-face <hughesbryan3000@gmail.com>
2024-09-18 16:59:51 -0400

    Refactor ModeToggle component to dynamically toggle theme icon based on current theme

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -1,23 +1,25 @@
 import { Moon, Sun } from "lucide-react";
 import { Theme, useTheme } from "remix-themes";
-import { Button } from "~/components/ui/button";
+import { Button } from "../app/components/ui/button";
 import {
     DropdownMenu,
     DropdownMenuContent,
     DropdownMenuItem,
     DropdownMenuTrigger,
-} from "~/components/ui/dropdown-menu";
+} from "../app/components/ui/dropdown-menu";
 
 export function ModeToggle() {
-    const [, setTheme] = useTheme();
+    const [theme, setTheme] = useTheme();
 
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
                 <Button variant="ghost" size="icon">
-                    <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                    <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                    <span className="sr-only">Toggle theme</span>
+                    {theme === "light" ? (
+                        <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                    ) : (
+                        <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                    )}
                 </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end">
