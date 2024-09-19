8c0440125c9315840327059681a92a0e5c8d5bb0
metal-face <hughesbryan3000@gmail.com>
2024-09-19 14:27:42 -0400

    border added to bottom

--- app/components/title-card.tsx
+++ app/components/title-card.tsx
@@ -2,7 +2,7 @@ import SocialButtons from "./social-buttons";
 
 export default function TitleCard() {
     return (
-        <div className="h-full w-full flex items-center justify-center">
+        <div className="h-full w-full flex items-center justify-center border-b">
             <div>
                 <h1 className="animate-fade my-name text-5xl sm:text-6xl md:text-8xl lg:text-9xl mb-2">
                     Bryan Hughes
