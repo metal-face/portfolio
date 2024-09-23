19b246f4835f1d389d3098a13ee07184f79f4ebd
metal-face <hughesbryan3000@gmail.com>
2024-09-22 20:49:32 -0400

    bash card made smaller

--- app/components/skills/bash.tsx
+++ app/components/skills/bash.tsx
@@ -10,10 +10,10 @@ export default function Bash(): ReactElement {
         <div className={"h-full w-full flex items-center justify-center"}>
             <Card
                 className={
-                    "h-[300px] w-[300px] flex items-center justify-center shadow-2xl dark:bg-neutral-600 dark:border-neutral-600"
+                    "h-[200px] w-[200px] flex items-center justify-center shadow-2xl dark:bg-neutral-600 dark:border-neutral-600"
                 }
             >
-                <CardContent className={"w-[250px] flex items-center justify-center p-0"}>
+                <CardContent className={"w-[150px] flex items-center justify-center p-0"}>
                     {theme === Theme.LIGHT ? (
                         <img src={BashLogoLight} alt={"Bash Logo"} />
                     ) : (
