ec848822781b4f1cb7af03bdf06f088fd2455c94
metal-face <hughesbryan3000@gmail.com>
2024-09-24 23:33:03 -0400

    error with pre not being able to be descendent of p fixed

--- app/components/about-me.tsx
+++ app/components/about-me.tsx
@@ -10,10 +10,8 @@ export default function AboutMe(): ReactElement {
             <div className={"col-span-full sm:col-span-2 sm:w-2/3 space-y-3 mx-4"}>
                 <span className={"about-me-title text-4xl sm:text-6xl md:text-7xl"}>About Me</span>
                 <p className={"about-me text-md sm:text-lg"}>
-                    My name is Bryan Hughes, although, some know me as{" "}
-                    <pre className={"inline"}>metalface</pre>. I was born and raised in Toronto,
-                    Canada. I am 32 years old and have been working as a Software Developer for 5
-                    years.
+                    My name is Bryan Hughes. I was born and raised in Toronto, Canada. I am 32 years
+                    old and have been working as a Software Developer for 5 years.
                 </p>
                 <p className={"about-me text-md sm:text-lg"}>
                     I am self taught, and constantly continuing my education by building personal
