904838a8b886844e9fe06db33a9373e2501c7739
metal-face <hughesbryan3000@gmail.com>
2024-09-25 23:49:40 -0400

    schedule route fleshed out a little more

--- app/routes/schedule.ts
+++ app/routes/schedule.ts
@@ -1,13 +1,48 @@
 import { ActionFunctionArgs, json } from "@remix-run/node";
 import { prisma } from "../../prisma";
 import type { ScheduleSchema } from "~/components/schedule-me";
+import { formSchema } from "~/components/schedule-me";
+import { toZonedTime } from "date-fns-tz";
+import { format } from "date-fns";
 
 export const action = async ({ request }: ActionFunctionArgs) => {
     switch (request.method) {
         case "POST": {
-            const data = await request.json();
+            const data: ScheduleSchema = await request.json();
+
             console.log(data);
-            return json({ success: true }, 200);
+
+            const localTime = toZonedTime(
+                data.scheduleTime,
+                Intl.DateTimeFormat().resolvedOptions().timeZone,
+            );
+
+            console.log(localTime);
+
+            const formattedTime = format(localTime, "yyyy-MM-dd HH:mm:ss");
+
+            const transformed = {
+                firstName: data.firstName,
+                lastName: data.lastName,
+                email: data.email,
+                scheduleDate: new Date(data.scheduleDate),
+                scheduleTime: new Date(data.scheduleTime),
+            };
+
+            try {
+                const isValid = await formSchema.safeParseAsync(transformed);
+
+                // const res = await prisma.scheduleRequest.create({
+                //     data: data,
+                // });
+
+                if (isValid) {
+                    return json({ success: true }, 200);
+                }
+            } catch (error: any) {
+                console.error(error);
+                return json({ success: false }, 500);
+            }
         }
     }
 };
