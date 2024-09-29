3b1c5cd8331dc74467457eeda527307a08052808
metal-face <hughesbryan3000@gmail.com>
2024-09-29 01:33:26 -0400

    favicon.ico links added to root

--- app/root.tsx
+++ app/root.tsx
@@ -54,6 +54,10 @@ export function App() {
                     href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap"
                     rel="stylesheet"
                 />
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
 
                 <Meta />
                 <PreventFlashOnWrongTheme ssrTheme={Boolean(data.theme)} />
