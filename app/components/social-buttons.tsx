c56522681834a1b12adbbd200f9d84f885d21749
metal-face <hughesbryan3000@gmail.com>
2024-09-30 07:22:01 -0400

    social buttons refactored to look better across different screens

--- app/components/social-buttons.tsx
+++ app/components/social-buttons.tsx
@@ -1,51 +1,81 @@
 import { Tooltip, TooltipContent, TooltipTrigger } from "~/components/ui/tooltip";
 import { Link } from "@remix-run/react";
 import { Button } from "~/components/ui/button";
-import { File, Github, Pen } from "lucide-react";
+import { Github, Hammer, ScrollText } from "lucide-react";
 
 export default function SocialButtons() {
     return (
-        <div className={"flex justify-center space-x-2 mt-2"}>
-            <Tooltip>
-                <TooltipTrigger asChild>
-                    <Link
-                        target={"_blank"}
-                        to={"https://github.com/metal-face"}
-                        unstable_viewTransition
-                    >
-                        <Button size={"lg"} className={"dark:bg-white bg-black"}>
-                            <Github className={"dark:text-black"} />
-                        </Button>
-                    </Link>
-                </TooltipTrigger>
-                <TooltipContent>
-                    <p>GitHub</p>
-                </TooltipContent>
-            </Tooltip>
-            <Tooltip>
-                <TooltipTrigger asChild>
-                    <Link to={"/BRYAN_HUGHES_RESUME_2024.pdf"} target={"_blank"}>
-                        <Button size={"lg"} className={"dark:bg-white bg-black"}>
-                            <File className={"dark:text-black"} />
-                        </Button>
-                    </Link>
-                </TooltipTrigger>
-                <TooltipContent>
-                    <p>Download Resume</p>
-                </TooltipContent>
-            </Tooltip>
-            <Tooltip>
-                <TooltipTrigger asChild>
-                    <Link to={"https://blog-builder.com"} target={"_blank"}>
-                        <Button size={"lg"} className={"dark:bg-white bg-black"}>
-                            <Pen className={"dark:text-black"} />
-                        </Button>
-                    </Link>
-                </TooltipTrigger>
-                <TooltipContent>
-                    <p>Blog Builder</p>
-                </TooltipContent>
-            </Tooltip>
+        <div className={"mt-5 w-full flex items-center justify-center mx-auto"}>
+            <div
+                className={
+                    "space-y-2 md:space-y-0 md:space-x-2  flex flex-col md:flex-row items-center justify-center"
+                }
+            >
+                <div className={"w-full"}>
+                    <Tooltip>
+                        <TooltipTrigger asChild>
+                            <Link
+                                target={"_blank"}
+                                to={"https://github.com/metal-face"}
+                                unstable_viewTransition
+                            >
+                                <Button
+                                    size={"lg"}
+                                    className={"dark:bg-white bg-black w-[200px] description"}
+                                >
+                                    <div className={"w-1/4"}>
+                                        <Github className={"dark:text-black mr-4"} />
+                                    </div>
+                                    <div className={"w-3/4"}>GitHub</div>
+                                </Button>
+                            </Link>
+                        </TooltipTrigger>
+                        <TooltipContent>
+                            <p>GitHub</p>
+                        </TooltipContent>
+                    </Tooltip>
+                </div>
+                <div className={"w-full"}>
+                    <Tooltip>
+                        <TooltipTrigger asChild>
+                            <Link to={"/BRYAN_HUGHES_RESUME_2024.pdf"} target={"_blank"}>
+                                <Button
+                                    size={"lg"}
+                                    className={"dark:bg-white bg-black w-[200px] description"}
+                                >
+                                    <div className={"w-1/4"}>
+                                        <ScrollText className={"dark:text-black mr-4"} />
+                                    </div>
+                                    <div className={"w-3/4"}>My Resume</div>
+                                </Button>
+                            </Link>
+                        </TooltipTrigger>
+                        <TooltipContent>
+                            <p>Download Resume</p>
+                        </TooltipContent>
+                    </Tooltip>
+                </div>
+                <div className={"w-full"}>
+                    <Tooltip>
+                        <TooltipTrigger asChild>
+                            <Link to={"https://blog-builder.com"} target={"_blank"}>
+                                <Button
+                                    size={"lg"}
+                                    className={"dark:bg-white bg-black w-[200px] description"}
+                                >
+                                    <div className={"w-1/4"}>
+                                        <Hammer className={"dark:text-black mr-4"} />
+                                    </div>
+                                    <div className={"w-3/4"}>Blog Builder</div>
+                                </Button>
+                            </Link>
+                        </TooltipTrigger>
+                        <TooltipContent>
+                            <p>Blog Builder</p>
+                        </TooltipContent>
+                    </Tooltip>
+                </div>
+            </div>
         </div>
     );
 }
