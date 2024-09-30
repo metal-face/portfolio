9de03f987a6a1fe1b8bc0e8479dbfa6df4682c04
metal-face <hughesbryan3000@gmail.com>
2024-09-30 04:46:57 -0400

    overflow hidden on the x-axis

--- app/root.tsx
+++ app/root.tsx
@@ -57,14 +57,13 @@ export function App() {
                 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-                <link rel="manifest" href="/site.webmanifest" />
 
                 <Meta />
                 <PreventFlashOnWrongTheme ssrTheme={Boolean(data.theme)} />
                 <Links />
             </head>
             <body>
-                <main className="h-screen w-screen">
+                <main className={"overflow-x-hidden"}>
                     <TitleCard />
                     <AboutMe />
                     <MySkills />
