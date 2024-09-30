c9b830521a4ad3a1224827b7686d809972c5543f
metal-face <hughesbryan3000@gmail.com>
2024-09-30 07:22:14 -0400

    title made larger across different screens

--- app/components/title-card.tsx
+++ app/components/title-card.tsx
@@ -4,7 +4,7 @@ export default function TitleCard() {
     return (
         <div className="h-screen w-screen flex items-center justify-center">
             <div>
-                <h1 className="animate-fade my-name text-5xl sm:text-6xl md:text-8xl lg:text-9xl mb-2">
+                <h1 className="animate-fade my-name text-6xl sm:text-7xl md:text-8xl lg:text-9xl mb-2 text-center">
                     Bryan Hughes
                 </h1>
                 <h2 className="description text-center text-md sm:text-lg md:text-xl lg:text-2xl ">
