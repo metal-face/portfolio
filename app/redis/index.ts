da51c3825795a075857dacc13897aec1cf286c52
metal-face <hughesbryan3000@gmail.com>
2024-10-01 19:55:42 -0400

    redis configuration changed

--- app/redis/index.ts
+++ app/redis/index.ts
@@ -1,7 +1,9 @@
 import IORedis from "ioredis";
 
 export const redisConnection: IORedis = new IORedis({
-    host: "76.76.21.21",
+    host: process.env.KV_HOST,
     port: 6379,
+    username: process.env.KV_USERNAME,
+    password: process.env.KV_PASSWORD,
     maxRetriesPerRequest: null,
 });
