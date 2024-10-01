1e816e39029096ee67776d2d485183285cf9412f
metal-face <hughesbryan3000@gmail.com>
2024-10-01 16:39:34 -0400

    redis configuration adjusted for deployment

--- app/redis/index.ts
+++ app/redis/index.ts
@@ -1,7 +1,7 @@
 import IORedis from "ioredis";
 
 export const redisConnection: IORedis = new IORedis({
-    host: "bryan-hughes.com",
+    host: "76.76.21.21",
     port: 6379,
     maxRetriesPerRequest: null,
 });
